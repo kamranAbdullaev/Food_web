89880c1b3c9c7054f075b742c468a8f7368cefff
kamran <kamranabdullaev4002@gmail.com>
2022-08-29 05:15:19 -0400

    add js

--- js/script.js
+++ js/script.js
@@ -732,6 +732,40 @@ window.addEventListener('DOMContentLoaded', function () {
 
   const indicators = document.createElement('ol');
   indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 15;
+      display: flex;
+      justify-content: center;
+      margin-right: 15%;
+      margin-left: 15%;
+      list-style: none;
+  `;
+slider.append(indicators);
+
+for(let i = 0;  i<slides.length; i++) {
+  const dor = document.createElement('li');
+  dot.setAttribute('data-slide-to', i + 1); //.setAttribute('имя_атрибута', 'значение_атрибута') – устанавливает указанный атрибут с указанным значением элементу. Если указанный атрибут есть у элемента, то данный метод тогда просто изменит ему значение.
+  dot.style.cssText = `
+      box-sizing: content-box;
+      flex: 0 1 auto;
+      width: 30px;
+      height: 6px;
+      margin-right: 3px;
+      margin-left: 3px;
+      cursor: pointer;
+      background-color: #fff;
+      background-clip: padding-box;
+      border-top: 10px solid transparent;
+      border-bottom: 10px solid transparent;
+      opacity: .5;
+      transition: opacity .6s ease;
+  `;
+  indicators.append(dot);
+}
 
   next.addEventListener('click', function () {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
